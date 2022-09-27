f1ebc9c0fb462b255a37fdfb79bc1dd9547957db
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-09-27 12:01:40 +0200

    example for bool convertion

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ fake_items_db = [{"item_name": "1 Foo"}, {"item_name": "2 Bar"}, {"item_name": "
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
 
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
 # *************************************************************************************
 # FIRST STEP OF THE TUTORIAL PASTAPI
 #
