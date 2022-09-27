f71b91c286e3a51613648a45f0234e56570e2b80
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-09-27 12:13:44 +0200

    comment second step on query params and start request body

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "1 Foo"}, {"item_name": "2 Bar"}, {"item_name": "3 Baz"}, {"item_name": "4 Foo"}, {"item_name": "5 Bar"}, {"item_name": "6 Baz"}, {"item_name": "7 Foo"}, {"item_name": "8 Bar"}, {"item_name": "9 Baz"}, {"item_name": "10 Foo"}, {"item_name": "11 Bar"}, {"item_name": "12 Baz"}, {"item_name": "13 Foo"}, {"item_name": "14 Bar"}, {"item_name": "15 Baz"}, {"item_name": "16 Foo"}, {"item_name": "17 Bar"}, {"item_name": "18 Baz"}, {"item_name": "19 Foo"}, {"item_name": "20 Bar"}, {"item_name": "21 Baz"},{"item_name": "22 Foo"}, {"item_name": "23 Bar"}, {"item_name": "24 Baz"} ]
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-
+# *************************************************************************************
+# SECOND STEP OF THE TUTORIAL FASTAPI
+#
+# from fastapi import FastAPI
+#
+# app = FastAPI()
+#
+# fake_items_db = [{"item_name": "1 Foo"}, {"item_name": "2 Bar"}, {"item_name": "3 Baz"}, {"item_name": "4 Foo"}, {"item_name": "5 Bar"}, {"item_name": "6 Baz"}, {"item_name": "7 Foo"}, {"item_name": "8 Bar"}, {"item_name": "9 Baz"}, {"item_name": "10 Foo"}, {"item_name": "11 Bar"}, {"item_name": "12 Baz"}, {"item_name": "13 Foo"}, {"item_name": "14 Bar"}, {"item_name": "15 Baz"}, {"item_name": "16 Foo"}, {"item_name": "17 Bar"}, {"item_name": "18 Baz"}, {"item_name": "19 Foo"}, {"item_name": "20 Bar"}, {"item_name": "21 Baz"},{"item_name": "22 Foo"}, {"item_name": "23 Bar"}, {"item_name": "24 Baz"} ]
+#
+#
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
+#
+#
 # @app.get("/items/{item_id}")
 # async def read_item(item_id: str, q: str | None = None, short: bool = False):
 #     item = {"item_id": item_id}
@@ -20,30 +42,29 @@ async def read_item(skip: int = 0, limit: int = 10):
 #             {"description": "This is an amazing item that has a long description"}
 #         )
 #     return item
-
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-
-@app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str):
-    item = {"item_id": item_id, "needy": needy}
-    return item
-
-
+#
+#
+# @app.get("/users/{user_id}/items/{item_id}")
+# async def read_user_item(
+#     user_id: int, item_id: str, q: str | None = None, short: bool = False
+# ):
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+#
+#
+# @app.get("/items/{item_id}")
+# async def read_user_item(item_id: str, needy: str):
+#     item = {"item_id": item_id, "needy": needy}
+#     return item
+#
 # *************************************************************************************
-# FIRST STEP OF THE TUTORIAL PASTAPI
+# FIRST STEP OF THE TUTORIAL FASTAPI
 #
 # from enum import Enum
 #
