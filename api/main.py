5843046f2ba88faaa602d67209f593bc1e1a39be
Massimiliano <91194651+iEpsiiloWz@users.noreply.github.com>
2022-09-28 14:41:06 +0200

    merge with main

--- api/main.py
+++ api/main.py
@@ -1,3 +1,23 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+import json
 
 app = FastAPI()
+
+class current_game(BaseModel):
+    username: str
+    password: str
+    score: float
+
+@app.get("/questions")
+def read_questions():
+    with open("questions.json", "r") as f:
+        questions = json.load(f)
+    return questions
+
+@app.post("/upload")
+async def upload_score(current_score: current_game ):
+    if 
+    return current_score
