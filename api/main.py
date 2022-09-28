f13ae26b641e7a2f6d5b8150ca9275bb57021941
Ruiseki <jojobru60@gmail.com>
2022-09-28 14:27:12 +0200

    /login

--- api/main.py
+++ api/main.py
@@ -1,3 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import json
 
+usersFile = open("users.json")
+users = json.load(usersFile)
 app = FastAPI()
+
+class login(BaseModel) :
+    pseudo : str
+    password : str
+
+@app.post("/login")
+async def login_user(login: login) :
+    for user in users :
+        if login.pseudo == user['pseudo'] and login.password == user['password'] :
+            return user
+
+    return {}
+
+# @app.post("/register")
